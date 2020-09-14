9a0bddd3f08e475775e4c203cea3f2835506e9e8
Wills Blake <richwblake@gmail.com>
2020-09-14 19:22:47 -0400

    Created seed data for players

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create 10 Players to seed database
+
+Player.create(name: "KRS", score: 10000)
+Player.create(name: "WLS", score: 9500)
+Player.create(name: "BOI", score: 9000)
+Player.create(name: "NRM", score: 8500)
+Player.create(name: "DAD", score: 7000)
+Player.create(name: "MOM", score: 6000)
+Player.create(name: "LOL", score: 4500)
+Player.create(name: "PKA", score: 3000)
+Player.create(name: "OOF", score: 1000)
+Player.create(name: "LSR", score: 1)
